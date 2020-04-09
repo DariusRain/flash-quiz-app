2e5d575ff9333d7278fff40b1fa51b21964d0f01
DariusRain <dariusrain.student@careerdevs.com>
2020-04-09 13:07:35 -0400

    FD

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import './App.css';
 import Header from './components/main/Header'
 import MainContent from './components/main/MainContent'
 import Footer from './components/main/Footer'
+// import {Provider} from 'react-redux';
+
 // React & ReactDom would be imported at the top of this document for this method to work.
 
 
@@ -11,12 +13,13 @@ export default class App extends Component {
   
   render() {
     return (
-
-      <div className="App">
-      <Header title="React Quiz App" />
-      <MainContent />
-      <Footer footerText="Copyright &copy; 2020 Quizme"/>
-    </div>
+      // <Provider store={store}>
+        <div className="App">
+          <Header title="React Quiz App" />
+          <MainContent />
+          <Footer footerText="Copyright &copy; 2020 Quizme"/>
+        </div>
+      // </Provider>
     )
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,11 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import {createStore} from 'redux';
+
+// Redux
+// const reducer = (state = {}, action) => {
+//   switch(action.type) {
+//     case "":
+//       break;
+//     case "":
+//       break;
+//     default:
+//       break;
+//   }
+// }
+
+// let store = createStore(reducer)
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  // <React.StrictMode>
+    <App />,
+  // </React.StrictMode>,
   document.getElementById('root')
 );
 
