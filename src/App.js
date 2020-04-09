ca17cad0190baafb15850ab21061f4f98d1776aa
DariusRain <dariusrain.student@careerdevs.com>
2020-04-09 14:27:34 -0400

    Adding changes then checking out

--- src/App.js
+++ src/App.js
@@ -3,7 +3,28 @@ import './App.css';
 import Header from './components/main/Header'
 import MainContent from './components/main/MainContent'
 import Footer from './components/main/Footer'
-// import {Provider} from 'react-redux';
+// Redux
+import {Provider} from 'react-redux';
+import {createStore} from 'redux';
+
+const defaultState = {
+  myQuizArray: [],
+  globalQuizArray: []
+}
+
+// Redux
+const reducer = (state = defaultState, action) => {
+  switch(action.type) {
+    case "ADD_QUIZ":
+      break;
+    case "":
+      break;
+    default:
+      break;
+  }
+}
+
+let store = createStore(reducer)
 
 // React & ReactDom would be imported at the top of this document for this method to work.
 
@@ -13,13 +34,13 @@ export default class App extends Component {
   
   render() {
     return (
-      // <Provider store={store}>
+      <Provider store={store}>
         <div className="App">
           <Header title="React Quiz App" />
           <MainContent />
           <Footer footerText="Copyright &copy; 2020 Quizme"/>
         </div>
-      // </Provider>
+    </Provider>
     )
   }
 }
